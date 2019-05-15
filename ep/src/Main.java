7594492df5548ba50f9d977bc6be53b5f9bb70bf
gabriel <trotogamer@gmail.com>
2019-05-15 08:42:15 -0300

    captura de text area para file concluida

--- ep/src/Main.java
+++ ep/src/Main.java
@@ -1,10 +1,14 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.JFrame;
 
 //classe mestra o Main que manda em tudo
 public class Main {
-    public static void main(String[] args) {
-        NewJFrame frame = new NewJFrame();
+    public static void main(String[] args) throws IOException {
+        Frame frame = new Frame();
         frame.setVisible(true);
+
     }
 
 }
