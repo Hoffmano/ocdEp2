6131a190a5f60afca9aa8fc3cd29eb33e5f06249
gabriel <trotogamer@gmail.com>
2019-05-14 21:59:55 -0300

    Inicio do projeto. Estruturacao da GUI

---
+++ ep/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+//classe mestra o Main que manda em tudo
+public class Main {
+    public static void main(String[] args) {
+        NewJFrame frame = new NewJFrame();
+        frame.setVisible(true);
+    }
+
+}
